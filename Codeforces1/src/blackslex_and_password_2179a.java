f54dd85cb6f8dbce8c5edd34b83104bb13ffe1e8
ritish8090 <ritishkumar8090@gmail.com>
2026-02-28 20:31:41 +0530

    blackslex_and_password_2179a added

---
+++ Codeforces1/src/blackslex_and_password_2179a.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class blackslex_and_password_2179a {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+            while (t-- > 0) {
+                int k = sc.nextInt();
+                int x = sc.nextInt();
+
+                int n = k * x + 1;
+                System.out.println(n);
+            }
+
+            sc.close();
+        }
+}
