754050163af556120738f96500ffb5c122549bfb
ritish8090 <ritishkumar8090@gmail.com>
2026-04-08 20:59:32 +0530

    TheEqualizer_2217A added

--- Codeforces1/src/TheEqualizer_2217A.java
+++ Codeforces1/src/TheEqualizer_2217A.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class TheEqualizer_A {
+public class TheEqualizer_2217A {
         public static void main(String[] args) throws IOException {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int t = Integer.parseInt(br.readLine().trim());
