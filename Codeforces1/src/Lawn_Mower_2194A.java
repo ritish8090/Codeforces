d6cff693a82c31c07d5a6ead93ab9a0bc4074a7b
ritish8090 <ritishkumar8090@gmail.com>
2026-07-02 14:05:39 +0530

    Another_Puzzle_from_Papyrus_2238A added

---
+++ Codeforces1/src/Lawn_Mower_2194A.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Lawn_Mower_2194A {
+        public static void main(String[] args) throws Exception {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            int t = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
+
+            while (t-- > 0) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                long n = Long.parseLong(st.nextToken());
+                long w = Long.parseLong(st.nextToken());
+
+                sb.append(n - n / w).append('\n');
+            }
+
+            System.out.print(sb);
+        }
+}
