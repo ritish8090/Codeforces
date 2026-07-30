9767367ddadfbde3179327f48f41dcf762419885
ritish8090 <ritishkumar8090@gmail.com>
2026-07-30 22:26:23 +0530

    Skibidus_and_Amog_u_2065A added

---
+++ Codeforces1/src/Skibidus_and_Amog_u_2065A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Skibidus_and_Amog_u_2065A {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+
+            while (t-- > 0) {
+                String s = sc.next();
+                System.out.println(s.substring(0, s.length() - 2) + "i");
+            }
+        }
+}
