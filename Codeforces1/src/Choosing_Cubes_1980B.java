aa3ff7c175855a1d202ce79b763d8b3cc7200455
ritish8090 <ritishkumar8090@gmail.com>
2026-08-27 23:05:40 +0530

    Choosing_Cubes_1980B added

---
+++ Codeforces1/src/Choosing_Cubes_1980B.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Choosing_Cubes_1980B {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int t = sc.nextInt();
+
+            while (t-- > 0) {
+                int n = sc.nextInt();
+                int f = sc.nextInt();
+                int k = sc.nextInt();
+
+                int[] a = new int[n];
+
+                for (int i = 0; i < n; i++) {
+                    a[i] = sc.nextInt();
+                }
+
+                int fav = a[f - 1];
+                int greater = 0;
+                int equal = 0;
+
+                for (int x : a) {
+                    if (x > fav) {
+                        greater++;
+                    } else if (x == fav) {
+                        equal++;
+                    }
+                }
+
+                if (greater >= k) {
+                    System.out.println("NO");
+                } else if (greater + equal <= k) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("MAYBE");
+                }
+            }
+        }
+}
