8fd5a98cab73344cca3b1740372c5a3751a83c3d
ritish8090 <ritishkumar8090@gmail.com>
2026-09-07 22:24:25 +0530

    Hit_the_Lottery_996A added

---
+++ Codeforces1/src/Hit_the_Lottery_996A.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Hit_the_Lottery_996A {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            int n = sc.nextInt();
+            int[] bills = {100, 20, 10, 5, 1};
+
+            int ans = 0;
+
+            for (int bill : bills) {
+                ans += n / bill;
+                n %= bill;
+            }
+
+            System.out.println(ans);
+        }
+}
